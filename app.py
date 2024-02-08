4dc77eab97e2712867b852daa1ce0edf943e6ece
Adam Southwick <adamsouthwick100@gmail.com>
2024-02-08 14:33:57 +0000

    changed environment, and added JCD API

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+import requests
+from dotenv import load_dotenv
 
 app = Flask(__name__, static_url_path='')
 
@@ -6,5 +9,16 @@ app = Flask(__name__, static_url_path='')
 def root():
     return app.send_static_file('index.html')
 
+def loadBikes():
+    load_dotenv()
+    apiKey = os.getenv('bike_key')
+    contract = 'Dublin'
+    apiRequest = requests.get(f'https://api.jcdecaux.com/vls/v1/stations?contract={contract}&apiKey={apiKey}')
+    print(apiRequest.status_code)
+    f = open("test_repo/dockInfo.json", "w")
+    f.write(apiRequest.text)
+    f.close()
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
+    loadBikes()
---
+++ test.py
@@ -0,0 +1,4 @@
+import os
+from flask import Flask, render_template
+import requests
+from dotenv import load_dotenv
